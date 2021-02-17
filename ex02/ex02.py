9ad6effdba37081ebfc45eef38c351d972fa3ea3
pacifastacus <palkovics.denes@gmail.com>
2021-02-17 15:00:16 +0100

    ex2.py updated

--- ex02/ex02.py
+++ ex02/ex02.py
@@ -3,7 +3,16 @@
 # 2 
 
 # 3
+def count_Lanister(f):
+	pass #Write your code here!
 
+try:
+	file = open("input.txt","r")
+	c = count_Lanister(file)
+	file.close()
+	print("A 'Lannister' szóv előfordulása a szövegben:{:d}".format(c))
+except FileNotFoundError:
+	print("Az input.txt állomány nem létezik!")
 # 4
 
 # 5
