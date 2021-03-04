de5485f6b3eb6bff94dd9876097a2c6abdf75845
pacifastacus <palkovics.denes@gmail.com>
2021-03-04 11:57:35 +0100

    otodik.py updated
    A more elegant solution given.
    Also negyedik.py updated in the comments.

--- ex04/negyedik.py
+++ ex04/negyedik.py
@@ -5,7 +5,7 @@ print(v)
 v.sort()
 print(v)
 
-# M = np.random.randint(0,100,(4,5))
+# M = np.random.randint(0,100,(4,5)) <- 4x5 mátrix
 # M.sort(axis=0) <- oszlopok szerinti rendezés
 # M.sort(axis=1) <- sorok szerinti rendezés
 # Megjegyzés: M.sort() = M.sort(axis=-1)
--- ex04/otodik.py
+++ ex04/otodik.py
@@ -2,5 +2,5 @@ import numpy as np
 
 v = np.arange(-5, 10)
 print(v)
-v[(v < 8) & (v > 3)] = v[(v < 8) & (v > 3)] * -1
+v[(v < 8) & (v > 3)] *= -1
 print(v)
